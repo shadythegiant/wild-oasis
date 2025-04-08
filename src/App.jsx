23626623217d940334854284970645a51ea6a64a
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-04-08 21:45:17 +0100

    setting up styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 31px;
+  font-weight: 400;
+  text-transform: capitalize;
+  letter-spacing: 5px;
+  text-align: center;
+`;
 function App() {
   return (
     <>
-      <div>Hello</div>
+      <div>
+        <H1>the Wild Oasis</H1>
+      </div>
     </>
   );
 }
