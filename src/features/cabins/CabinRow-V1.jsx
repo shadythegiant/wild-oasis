6364e65db69293faa03e24b999d30076b6b3ceee
shadythegiant <hellgatesdesigner2010@gmail.com>
2025-04-21 19:05:32 +0100

    creating a reusable table using compound component pattern

--- src/features/cabins/CabinRow-V1.jsx
+++ src/features/cabins/CabinRow-V1.jsx
@@ -7,7 +7,18 @@ import { HiPencil, HiSquare2Stack, HiTrash } from "react-icons/hi2";
 import useCreateCabin from "./useCreateCabin";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
-import Table from "../../ui/Table";
+
+const TableRow = styled.div`
+  display: grid;
+  grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
+  column-gap: 2.4rem;
+  align-items: center;
+  padding: 1.4rem 2.4rem;
+
+  &:not(:last-child) {
+    border-bottom: 1px solid var(--color-grey-100);
+  }
+`;
 
 const Img = styled.img`
   display: block;
@@ -79,7 +90,7 @@ export default function CabinRow({ cabin }) {
   // ------------------------------------------ jsx --------------------------------
   return (
     <>
-      <Table.Row>
+      <TableRow>
         <Img src={image} />
         <Cabin> {name}</Cabin>
         <div>fits up to {maxCapacity}</div>
@@ -122,7 +133,7 @@ export default function CabinRow({ cabin }) {
             <HiSquare2Stack />
           </button>
         </BtnContainer>
-      </Table.Row>
+      </TableRow>
     </>
   );
 }
